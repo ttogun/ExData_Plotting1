fbbbc7ab32b6a6b4d2dac55200a1aad179211182
ttogun <taiwo.togun@gmail.com>
2017-02-12 11:34:33 -0500

    added plot4.R and plot4.png

--- plot4.R
+++ plot4.R
@@ -18,13 +18,27 @@ rm(data)
 ## create a compound date-time variable 
 data <- within(relevantData, Datetime <- as.POSIXlt(paste(as.character(Date),as.character(Time)),format="%d/%m/%Y %H:%M:%S"))
 
-png(filename="Plot3.png", width=480, height=480)
+
+png(filename="Plot4.png", width=480, height=480)
+
+par(mfrow=c(2,2))
+with(data,plot(data$Datetime, as.numeric(as.character(Global_active_power)), type="l", ylab="Global Active Power (kilowatts)",
+               xlab=""))
+
+with(data,plot(data$Datetime, as.numeric(as.character(Voltage)), type="l", ylab="Voltage",
+               xlab="datetime"))
+
 
 with(data,plot(data$Datetime, as.numeric(as.character(Sub_metering_1)), type="l", ylab="Energy sub metering",
                xlab=""))
 with(data,points(data$Datetime, as.numeric(as.character(Sub_metering_2)), type="l", col="red"))
 with(data,points(data$Datetime, as.numeric(as.character(Sub_metering_3)), type="l", col="blue"))
-legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red","blue"), lty=1)
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red","blue"),lty=1)
+
+with(data,plot(data$Datetime, as.numeric(as.character(Global_reactive_power)), type="l", ylab="Global_reactive_power",
+               xlab="datetime"))
+
+
 
 dev.off()
 
