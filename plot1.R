b274142d74fcc1719af8139a1276d93d783c8504
ttogun <taiwo.togun@gmail.com>
2017-02-12 11:18:29 -0500

    add plot3.png and plot3.R

--- plot1.R
+++ plot1.R
@@ -4,17 +4,18 @@
 ####
 setwd("~/../Dropbox/Coursera/DataScience/ExploratoryAnalysis/ExData_Plotting1")
 ## load data
-
-#data<-read.table("./Datasets/household_power_consumption.txt", header=T, sep= ";")
+### assumes there's a folder in current directory called Datasets, which contains data of interest
+data<-read.table("./Datasets/household_power_consumption.txt", header=T, sep= ";")
 ## get relevant data
-#relevantDateIndx <-as.Date(data$Date,format="%d/%m/%Y")>=as.Date("2007-02-01") &
-#  as.Date(data$Date,format="%d/%m/%Y")<=as.Date("2007-02-02")
+relevantDateIndx <-as.Date(data$Date,format="%d/%m/%Y")>=as.Date("2007-02-01") & as.Date(data$Date,format="%d/%m/%Y")<=as.Date("2007-02-02")
 
-#relevantData <- data[relevantDateIndx,]
-#rm(data)
+relevantData <- data[relevantDateIndx,]
+rm(data)
 #save(relevantData, file="project1Data.Rdata")
 
-load("project1Data.Rdata")
+#load("project1Data.Rdata")
+
+#####make histogram plot
 png(filename="Plot1.png", width=480, height=480)
 
 with(relevantData, hist(as.numeric(as.character(Global_active_power)), col="red", xlab="Global Active Power (kilowatts)",
--- plot3.R
+++ plot3.R
@@ -18,9 +18,13 @@ rm(data)
 ## create a compound date-time variable 
 data <- within(relevantData, Datetime <- as.POSIXlt(paste(as.character(Date),as.character(Time)),format="%d/%m/%Y %H:%M:%S"))
 
-png(filename="Plot2.png", width=480, height=480)
+png(filename="Plot3.png", width=480, height=480)
 
-with(data,plot(data$Datetime, as.numeric(as.character(Global_active_power)), type="l", ylab="Global Active Power (kilowatts)",
+with(data,plot(data$Datetime, as.numeric(as.character(Sub_metering_1)), type="l", ylab="Energy sub metering",
                xlab=""))
+with(data,points(data$Datetime, as.numeric(as.character(Sub_metering_2)), type="l", col="red"))
+with(data,points(data$Datetime, as.numeric(as.character(Sub_metering_3)), type="l", col="blue"))
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red","blue"))
+
 dev.off()
 
